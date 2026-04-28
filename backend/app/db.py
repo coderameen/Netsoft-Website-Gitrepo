e8d3b497b0d89e11b03fe740c533ccbd58d37363
Mohammed Ameenul hasan <ameen@Mohammeds-MacBook-Pro.local>
2026-04-28 09:08:47 +0300

    added code

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./netsoft.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///./netsoft.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
